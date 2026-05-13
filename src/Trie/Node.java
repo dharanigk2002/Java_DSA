43cd79b4d6a6f5888f1a5df542beaa05b1759ff0
Eswar <dharanigk2018@gmail.com>
2026-05-13 09:35:00 +0530

    Initial commit

---
+++ src/Trie/Node.java
@@ -0,0 +1,31 @@
+package Trie;
+// Prefix tree ds
+public class Node {
+    Node[] child;
+    boolean end;
+
+    public Node() {
+        child=new Node[26];
+        end=false;
+    }
+
+    public boolean containsKey(char c) {
+        return child[c-97]!=null;
+    }
+
+    public void put(char ch) {
+        child[ch-97]=new Node();
+    }
+
+    public void setEnd() {
+        end=true;
+    }
+
+    public boolean isEnd() {
+        return end;
+    }
+
+    public Node get(char ch) {
+        return child[ch-97];
+    }
+}
