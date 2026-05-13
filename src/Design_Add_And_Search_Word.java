b4f64efcd21caf36f9ab6cd20260c09fbc41cc21
Eswar <dharanigk2018@gmail.com>
2026-05-13 08:36:50 +0530

    Trie ds-design add and search word

---
+++ src/Design_Add_And_Search_Word.java
@@ -0,0 +1,69 @@
+class Node {
+    Node[] child;
+    boolean end;
+
+    public Node() {
+        child=new Node[26];
+        end=false;
+    }
+
+    public boolean containsKey(char c) {
+        return child[c-97]!=null;
+    }
+
+    public void put(char ch) {
+        child[ch-97]=new Node();
+    }
+
+    public void setEnd() {
+        end=true;
+    }
+
+    public boolean isEnd() {
+        return end;
+    }
+
+    public Node get(char ch) {
+        return child[ch-97];
+    }
+}
+private Node root=null;
+void addWord(String word) {
+    Node temp=root;
+    for(char c:word.toCharArray()) {
+        if(!temp.containsKey(c))
+            temp.put(c);
+        temp=temp.get(c);
+    }
+    temp.setEnd();
+}
+
+boolean dfs(String s, int id, Node root) {
+    if(id==s.length())
+        return true;
+    char c=s.charAt(id);
+    if(c=='.') {
+        for(char ch='a';ch<='z';ch++)
+            if(root.containsKey(ch) && dfs(s, id+1, root.get(ch)))
+                return true;
+        return false;
+    }
+    if(!root.containsKey(c))
+        return false;
+    return dfs(s, id+1, root.get(c));
+}
+
+boolean search(String word) {
+    return dfs(word, 0, root);
+}
+
+void main() {
+    root=new Node();
+    addWord("bad");
+    addWord("dad");
+    addWord("mad");
+    IO.println("Word pad is"+(search("pad")?" found":" not found"));
+    IO.println("Word bad is"+(search("bad")?" found":" not found"));
+    IO.println("Word .ad is"+(search(".ad")?" found":" not found"));
+    IO.println("Word b.. is"+(search("b..")?" found":" not found"));
+}
