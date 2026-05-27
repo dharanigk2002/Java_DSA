1a0d44a8ba84fbdc1829272cd0299f436b87ab0b
Eswar <dharanigk2018@gmail.com>
2026-05-27 09:43:19 +0530

    Initial commit

---
+++ src/Graph/Center_of_star_graph.java
@@ -0,0 +1,21 @@
+package Graph;
+/*
+There is an undirected star graph consisting of n nodes labeled from 1 to n. A star graph is a graph where there is one center node and exactly n - 1 edges that connect the center node with every other node.
+
+You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates that there is an edge between the nodes ui and vi. Return the center of the given star graph.
+
+https://leetcode.com/problems/find-center-of-star-graph/description/
+*/
+public class Center_of_star_graph {
+    static int findCenter(int[][] edges) {
+        if(edges[0][0]==edges[1][0] || edges[0][0]==edges[1][1])
+            return edges[0][0];
+        return edges[0][1];
+    }
+
+    static void main() {
+        int[][] edges = {{1, 2}, {5, 1}, {1, 3}, {1, 4}};
+        int center = findCenter(edges);
+        IO.println(center);
+    }
+}
